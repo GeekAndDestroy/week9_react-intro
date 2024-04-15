f6f5d942d14b54277dc2fc5665efe78c18f9d7ce
Jeff Chebul <jmchebul@jmchebul.com>
2024-04-15 15:12:04 -0500

    Add Navigation component iwith isLoggedIn props

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
+import Navigation from './components/Navigation';
+
 
 export default function App(){
-  const firstName: string = 'Friedrick';
-  const lastName: string = 'Von Sticlburgen';
-  const isLoggedIn: boolean = false;
-  let heading: string;
-  // if (isLoggedIn){
-  //   heading = 'Welcome back'
-  // } else {
-  //     heading = 'Please Log In or Sign Up'
-  // }
+    const firstName: string = 'Jeff';
+    const lastName: string = 'Chebul';
+    const isLoggedIn: boolean = true;
 
-const posts: {id:number, title:string}[] = [
-  {id: 1, title: "Happy Monday"},
-  {id: 2, title: "React Rules!"},
-  {id: 3, title: "Spring has Sprung"}
-]
+    const posts: {id:number, title:string}[] = [
+        {id: 1, title: 'Happy Monday'},
+        {id: 2, title: 'React Rules!'},
+        {id: 3, title: 'Spring has Sprung'}
+    ]
 
-
-
-  return (
-      <div>
-          <h1>Hello World</h1>
-          <h2>{isLoggedIn ? `Welcome back${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
-          {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
-      </div>
-  )
+    return (
+        <>
+            <Navigation isLoggedIn={isLoggedIn}/>
+            <h1>Hello World</h1>
+            <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
+            {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+        </>
+    )
 }
