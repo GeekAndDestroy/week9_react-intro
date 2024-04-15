ace904f3d9ceda09d137e36432903896c9b48e39
Jeff Chebul <jmchebul@jmchebul.com>
2024-04-15 15:59:59 -0500

    Add styling via our own CSS + React-Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css'; // Import our own CSS
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
